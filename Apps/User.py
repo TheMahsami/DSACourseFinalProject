3dfeb184bae40cc5cb99b2ba196a49576dae1681
TheBlue <ma.sh106mahsami@gmail.com>
2025-06-16 02:36:56 +0430

    is valid plate number finished

--- Apps/User.py
+++ Apps/User.py
@@ -1,7 +1,11 @@
+import sys , os
+sys.path.append(os.path.abspath('.'))
+import random
 from Modules import read_city_codes
+from DataSets.Array import Array
 
 class User:
-    def __init__(self,fname,lname,ncode,password):
+    def __init__(self,fname="",lname="",ncode="",password=''):
         self.id = ncode
         self.ncode = ncode
         self.name = fname
@@ -9,5 +13,64 @@ class User:
         self.password = password
         self.cars = None
         
-    def _license_plate_generator(request , citycode ):
+    def license_plate_generator(self , cityname ):
+        all_city_codes = read_city_codes()
+        city_code = all_city_codes.search(cityname)
+        if not city_code:
+            return 'this city is unknown please chiise a valid city'
         
+        allowed_letters = 'ABCEFGHIJKLMNOQRSTUVWXYZ'
+        letter = random.choice(allowed_letters)
+        
+        strnumbers =''
+        valid_number_flag = False
+        attempted_numbers_counter = 0
+        while not valid_number_flag and attempted_numbers_counter <6:
+            temp = ''.join(random.choice('0123456789' , k = 5))
+            if self._is_valid_plate_nummber(temp , letter):
+                strnumbers = temp
+                valid_number_flag = True
+                attempted_numbers_counter += 1
+        
+            if not valid_number_flag:
+                continue
+            
+            new_plate = f'{city_code}{letter}{strnumbers}'
+            
+    def _is_valid_plate_nummber(self , number):
+        print(number)
+        if self._has_repeated_digit(number) == 5:
+            return False
+        
+        #barresi soodi
+        strnumber = str(number)
+        ascendig_flag = True
+        for i in range(len(strnumber) - 1):
+            if strnumber[i+1] != int(strnumber[i]) + 1:
+                ascendig_flag = False
+                break
+            print(ascendig_flag)
+        if ascendig_flag:
+            return False
+        #barresi nozooli boodan
+        descendinf_flag = True
+        for i in range(len(strnumber) - 1):
+            if strnumber[i+1] != int(strnumber[i]) - 1:
+                descendinf_flag = False
+                break
+        if descendinf_flag:
+            return False
+        
+    def _has_repeated_digit(self,number):
+        number = str(number)
+        count = 0
+        for i in range(len(number)):
+            for j in range(i+1 , len(number)):
+                
+                if number[i] == number[j]:
+                    count +=1
+                    break
+        return count
+
+user= User()
+print(user._is_valid_plate_nummber(123456))
